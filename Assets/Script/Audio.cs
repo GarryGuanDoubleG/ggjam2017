f94710088a51fa28f6144169c14b7dc7ab31dcbe
Garry Guan <gg99@njit.edu>
2017-01-21 01:04:23 -0500

    initial commit tracking all relevant files

---
+++ Assets/Script/Audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+//container class that stores audio clip to be played
+public class Sound  : MonoBehaviour
+{
+    AudioSource sound;
+
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        sound.playOnAwake = false;
+    }
+
+    void OnCollisionEnter()
+    {
+        sound.Play();
+    }
+
+}
